d25f5d2818d1f93210691d21d558734452e3f5b4
NK <nkkd0348@gmail.com>
2022-03-23 22:17:20 -0700

    attach solution for 0x06-pointers_arrays_strings project

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,18 @@
  */
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		x = -x;
+	}else
+	{
+		n1 = n;
 	}
-	if ((x / 10) > 0)
-		print_number(x / 10);
+	if (n1 / 10)
+		print_number(n1 / 10);
 
-	_putchar(x % 10 + '0');
+	_putchar(n1 % 10 + '0');
 }
